4a9d4e4b58a4bc4acc9310ac9cc059ee2e220f61
Matthew Wardle <mwardle.22@gmail.com>
2019-10-22 20:30:40 +0000

    Refactor function to properly work with data via callbacks

---
+++ asyncBreeds.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log('Callback: I have the data!')
+    if (!error) callback(data); 
+  });
+}
+
+breedDetailsFromFile('Bombay', (data) => {
+  console.log(`Return value: ${data}`);
+});
+
+
+
+
+
+
+
+
+
+
+
