566f39c307795132ae745b469e36bb44b84a6149
Matthew Wardle <mwardle.22@gmail.com>
2019-10-22 21:07:04 +0000

    Added tests using Mocha and Chai for both syncBreeds.js and asyncBreeds.js

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,13 +3,15 @@ const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log('Callback: I have the data!')
-    if (!error) callback(data); 
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    } 
   });
 }
 
-breedDetailsFromFile('Bombay', (data) => {
-  console.log(`Return value: ${data}`);
-});
+module.exports = breedDetailsFromFile;
 
 
 
